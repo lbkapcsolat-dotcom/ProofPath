6d2e7d789c4f986006502d9aeede8b3421be17da
lbkapcsolat-dotcom <lb.kapcsolat@gmail.com>
2026-09-13 14:07:37 +0200

    test(formal): add mathlib bridge build root

--- formal/namespace-safe-eq64/lakefile.lean
+++ formal/namespace-safe-eq64/lakefile.lean
@@ -9,4 +9,4 @@ require mathlib from git
 
 @[default_target]
 lean_lib NamespaceSafeEQ64 where
-  roots := #[`NamespaceSafeEQ64, `HomologyGate, `AbelianHomologyGate, `NatIndexedHomologyGate, `CrossUniverseHomologyGate]
+  roots := #[`NamespaceSafeEQ64, `HomologyGate, `AbelianHomologyGate, `NatIndexedHomologyGate, `CrossUniverseHomologyGate, `MathlibHomologicalComplexBridge]
